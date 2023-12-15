2db64768f679b8bda2681f2a0df892d33b202f13
Talha Noman <talhanoman61@gmail.com>
2023-12-16 02:46:04 +0500

    Floating Doubt Submission

--- src/components/FloatingButton/floatingButton.jsx
+++ src/components/FloatingButton/floatingButton.jsx
@@ -1,9 +1,12 @@
 import React, { useState, useRef } from 'react'
 import Overlay from 'react-bootstrap/Overlay';
 // import { MdOutlineEmail } from "react-icons/md";
-import { User11, logo } from '../imagepath'
-import { MessageBox, Input, ChatItem } from 'react-chat-elements';
+import { logo } from '../imagepath'
+import { CopyToClipboard } from 'react-copy-to-clipboard';
 import 'react-chat-elements/dist/main.css';
+import FileUploader from '../FileUploader';
+import ReactTagsInput from '../pages/course/addCourse/tags';
+import { Link } from 'react-router-dom';
 export default function FloatingButton() {
     const [show, setShow] = useState(false);
     const target = useRef(null);
@@ -26,13 +29,17 @@ export default function FloatingButton() {
                         {...props}
                         style={{
                             position: 'absolute',
-                            backgroundColor: 'rgba(10,19,63, 0.8)',
-                            padding: '2px 2px',
+                            boxShadow: '0 8px 8px -4px #95abbb',
+                            // backgroundColor: '#FBFCFF',
+                            // padding: '2px 2px',
                             borderRadius: 3,
+                            zIndex: 1000000000,
                             ...props.style,
                         }}
                     >
-                        <ChatScreen />
+                        <div className="card">
+                            <DoubtScreen />
+                        </div>
                     </div>
                 )}
             </Overlay>
@@ -41,75 +48,114 @@ export default function FloatingButton() {
 }
 
 
-const ChatScreen = () => {
-    const inputRef = useRef(null);
-    const [messages, setMessages] = useState([
-        {
-            position: 'left',
-            type: 'text',
-            text: 'Hello, how can I help you?',
-        },
-        {
-            position: 'right',
-            type: 'text',
-            text: 'Hi! I have a question about your products.',
-        },
-    ]);
+const DoubtScreen = () => {
+    const textareaRef = useRef(null);
+    const [pastedImages, setPastedImages] = useState([]);
+    const handlePaste = (event) => {
+        const clipboardData = event.clipboardData || window.clipboardData;
+        const items = clipboardData.items;
 
-    const handleSendMessage = (text) => {
-        const newMessage = {
-            position: 'right',
-            type: 'text',
-            text,
-        };
+        const newPastedImages = [];
 
-        setMessages([...messages, newMessage]);    
+        for (let i = 0; i < items.length; i++) {
+            const item = items[i];
+
+            if (item.type.indexOf('image') !== -1) {
+                const blob = item.getAsFile();
+                const reader = new FileReader();
+
+                reader.onloadend = () => {
+                    // Handle the image data (e.g., display it, upload it, etc.)
+                    console.log('Pasted Image Data:', reader.result);
+                    newPastedImages.push(reader.result);
+                    setPastedImages([...pastedImages, ...newPastedImages]);
+                };
+
+                reader.readAsDataURL(blob);
+            }
+        }
     };
 
+
+
     return (
-        <div className='rounded-md' style={{ maxWidth: '400px', margin: 'auto' }}>
-            <ChatItem
-                avatar={User11}
-                alt={'John Doe'}
-                title={'John Doe'}
-                subtitle={'Support Team Representative'}
-            />
 
-            <MessageBox
-                position={'left'}
-                type={'text'}
-                text={'Hello, how can I help you?'}
-            />
+        <div className='rounded-md card-body text-white' style={{ width: '700px', margin: 'auto', zIndex: 1000000000 }}>
 
-            {messages.map((message, index) => (
-                <MessageBox key={index} {...message} />
-            ))}
-
-            {/* Input area for sending new messages */}
-            <hr className='text-white' />
-            <div className="d-flex mw-100 align-items-center pb-2">
-                <div className=" w-75 px-2">
+            <h4 className='text-center text-white bg-orange py-3 rounded' style={{
+                opacity: 0.9
+            }}>Doubt Submission</h4>
+            <div className='p-3'>
+                <div className="form-group">
+                    <label className='text-sm'>Title</label>
                     <input
-                        ref={inputRef}
                         type="text"
-                        className="form-control-sm w-100"
-                        placeholder="Write Message"
-                        onKeyDown={(e) => {
-                            if (e.key === 'Enter') {
-                                handleSendMessage(e.target.value);
-                                e.target.value = '';
-                            }
-                        }}
+                        className="form-control"
+                        placeholder="Enter Doubt Title"
+                        required
                     />
-                </div>             
-                <button className='w-25 btn-sm btn-primary me-2' onClick={() => {
-                    handleSendMessage(inputRef.current.value)
-                    inputRef.current.value = ''
-                }}>
-                    Send
-                </button>
-            </div>
+                </div>
+                <CopyToClipboard text="" onCopy={() => console.log('Text copied')}>
+                    <div className="form-group">
+                        <label className='text-sm'>Doubt Description</label>
+                        <textarea
+                            ref={textareaRef}
+                            onPaste={handlePaste}
+                            type="text"
+                            className="form-control"
+                            placeholder="Enter Doubt Description"
+                            required
+                            rows={4}
+                        ></textarea>
+                    </div>
+                </CopyToClipboard>
 
+                {
+                    pastedImages !== null
+                    &&
+                    <div className='d-flex' >
+                        {pastedImages?.map((image, index) => (
+                            <div key={index} className="me-3">
+                                <img
+                                    src={image}
+                                    className="img-thumbnail img-fluid"
+                                    alt={`Pasted from Clipboard ${index + 1}`}
+                                    width={80}
+                                    height={80}
+                                    style={{
+                                        aspectRatio: '1',
+                                        objectFit: 'contain',
+                                    }}
+                                />
+                            </div>
+                        ))}
+                    </div>
+                }
+                <div className='form-group'>
+                    <label className='text-sm'>Tags</label>
+                    <ReactTagsInput />
+                </div>
+                <div className="form-group">
+                    <div className="d-flex justify-content-between">
+                        <label className='text-sm'>Upload Attachment &lt;5MB</label>
+                        <label className='text-sm text-end small'>Upto 3 Attachments</label>
+                    </div>
+                    <FileUploader />
+
+                </div>
+                {/* <div className='w-100 text-center'>
+                <button className='w-25 btn-sm btn-primary me-2'>
+                    Submit
+                </button>
+            </div> */}
+                <div className="d-flex justify-content-center">
+                    <Link to="#" className="discover-btn" style={{
+                        opacity: 0.9
+                    }}>
+                        Submit  <i className="fas fa-paper-plane ms-2"></i>
+                    </Link>
+                </div>
+            </div>
         </div>
     );
 };
